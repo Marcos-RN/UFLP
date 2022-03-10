9f3aa922598bc230da65596b94931b0a2fce9c1a
Marcos-RN <marcosro1999@gmail.com>
2022-03-10 18:23:47 +0100

    Actualizacion

--- src/main/scala/Instance.scala
+++ src/main/scala/Instance.scala
@@ -1,54 +1,74 @@
-import java.io.File
+import java.io._
 
-class Instance (NF: Int , NL: Int , OC: Array[Int] , SC : Array[Array[Int]]) {
+class Instance (numFacilities: Int , numLocations: Int , openCost: Array[Double] , serviceCost : Array[Array[Double]]) {
 
-  val openCost: Array[Int] = OC
-  val serviceCost: Array[Array[Int]] = SC
-  val NumFacilities: Int = NF
-  val NumLocations: Int = NL
+  override def toString: String = s"Instance Number of facilities = $numFacilities , Instances Number of locations = $numLocations"
 
+  def toFile(nombreFich: String): Unit = {
+    val fich = new File(nombreFich)
+    val ps = new PrintStream(fich)
+    ps.println(numFacilities)
+    ps.println(numLocations)
+    ps.println()
+    ps.println("{")
+    for (i <- 0 until numFacilities) {
+      ps.print(openCost(i))
+    }
+    ps.println("}")
+    ps.println()
+    ps.println("{")
+    for (i <- 0 until numLocations) {
+      ps.println("{")
+      for (j <- 0 until numFacilities){
+        ps.print(serviceCost(i)(j))
+      }
+      ps.println("}")
+    }
+    ps.close()
+  }
 
-  override def toString: String = s"Instance Number of facilities = $NumFacilities , Instances Number of locations = $NumLocations"
+}
 
-  object Leer extends App {
+  object Instance extends App {
+
     import java.util.Scanner
 
-    def parametros(nombreFich : String) : (Int,Int,Array[Double],Array[Array[Double]]) = {
+    def fromFileOrLib(nombreFich: String): Unit = {
       var NF = 0
       var NL = 0
-      var OC = Array[Double]()
-      var SC = Array[Array[Double]]()
       val fich = new File(nombreFich)
       val sc = new Scanner(fich)
       NF = sc.nextInt()
       NL = sc.nextInt()
-      var i = 1
-      while (i <= NF) {
+      var OC = Array.ofDim[Double](NF)
+      var i = 0
+      while (i < NF) {
         val line = sc.nextLine()
         val scLn = new Scanner(line)
-        scLn.next()
-        OC = OC ++ Array[Double](scLn.nextDouble())
+        OC(i) = scLn.nextDouble()
         i += 1
         scLn.close()
       }
-      var j = 1
-      while (j <= NL) {
+      OC
+      var SC = Array.ofDim[Double](NL, NF)
+      var j = 0
+      while (j < NL) {
         sc.nextLine()
-        var L = Array[Double]()
         val line = sc.nextLine()
         val scLn = new Scanner(line)
-        var k = 1
-        while (k <= NF) {
-          L = L ++ Array[Double](scLn.nextDouble())
-          k +=1
+        var k = 0
+        while (k < NF) {
+          SC(j)(k) = scLn.nextDouble()
+          k += 1
         }
-        SC = SC ++ L
         j += 1
       }
       sc.close()
-      (NF,NL,OC,SC)
+      //return Instance(NF, NL, SC, OC)
     }
+
   }
 
 
-}
+
+
