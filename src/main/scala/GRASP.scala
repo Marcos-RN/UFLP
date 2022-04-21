5b152fae056ecb87b03ccb45a9abc5714fa6abf4
Marcos-RN <marcosro1999@gmail.com>
2022-04-21 21:54:08 +0200

    Este no funciona, hay que arreglarlo.

--- src/main/scala/GRASP.scala
+++ src/main/scala/GRASP.scala
@@ -4,6 +4,8 @@ import scala.collection.mutable.ArrayBuffer
 
 class GRASP (val instance: Instance) {
 
+  "NO FUNCIONA BIEN"
+
   def solve(n: Int, seed: Int) : Solution = {
     val solution = Array.ofDim[Boolean](instance.numLocations)
     val evalSol = Array.ofDim[Double](instance.numLocations)
@@ -54,41 +56,6 @@ class GRASP (val instance: Instance) {
     Solution.apply(solution)
   }
 
-  /*def solve(n: Int) : Solution = {
-    val solution = Array.ofDim[Boolean](instance.numLocations)
-    val evalSol = Array.ofDim[Double](instance.numLocations)
-    for (i <- 0 until  instance.numLocations) {
-      val sol = Array.ofDim[Boolean](instance.numLocations)
-      sol(i) = true
-      evalSol(i) = Solution.apply(sol).eval(instance)
-    }
-    val first = evalSol.min
-    solution(evalSol.indexOf(first)) = true
-    var c = 0
-    while (c == 0) {
-      val oldVal = Solution.apply(solution).eval(instance)
-      val evalSol2 = Array.ofDim[Double](instance.numLocations)
-      val arraySol = Array.ofDim[Array[Boolean]](instance.numLocations)
-      for (k <- arraySol.indices) {
-        arraySol(k) = solution
-      }
-      for (i <- arraySol.indices) {
-        arraySol(i)(i) = true
-        evalSol2(i) = Solution.apply(arraySol(i)).eval(instance)
-      }
-      val sortedEval = evalSol2.sorted
-      val minValues = sortedEval.take(n)
-      val seed = ((instance.openCost.sum)/(instance.numLocations)).toInt
-      val rnd = Random(seed)
-      val chosen = rnd.uniform(n)
-      val minVal = minValues(chosen)
-      //println(minVal)
-      if (minVal == oldVal) c += 1
-      else solution(evalSol2.indexOf(minVal)) = true
-      println(solution.mkString)
-    }
-    Solution.apply(solution)
-  }*/
 
 
 }
