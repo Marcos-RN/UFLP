3a5d7298e75262bf6d4a37956767b67e97d6f571
Marcos-RN <marcosro1999@gmail.com>
2022-04-27 20:03:07 +0200

    Casi terminado. (Fallo linea 58)

--- src/main/scala/GRASP.scala
+++ src/main/scala/GRASP.scala
@@ -1,61 +1,70 @@
 import util.random.xoShiRo256StarStar.Random
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.PriorityQueue
 
-class GRASP (val instance: Instance) {
+class GRASP (val instance: Instance , val rnd : Random) {
 
-  "NO FUNCIONA BIEN"
+  case class PQItem(index: Int, value : Double) extends Ordered[PQItem] {
+    def compare(that: PQItem): Int = -this.value.compare(that.value)
+  }
+
+  def min(a : Int, b: Int): Int = {
+    if (a < b) a
+    else b
+  }
 
   def solve(n: Int) : Solution = {
     val solution = Array.ofDim[Boolean](instance.numLocations)
     val evalSol = Array.ofDim[Double](instance.numLocations)
     val bestOption = Array.ofDim[Double](instance.numCustomers)
+    val pq = new  PriorityQueue[PQItem]()
     for (i <- 0 until instance.numLocations) {
       var sum = 0.0
       for (j <- 0 until instance.numCustomers) {
         sum += instance.serviceCost(j)(i)
       }
+      sum += instance.openCost(i)
       evalSol(i) = sum
+      pq.enqueue(PQItem(i,sum))
     }
-    val first = evalSol.min
-    println(first)
-    val ind = evalSol.indexOf(first)
-    solution(ind) = true
+    val m = min(n,instance.numLocations)
+    var first = PQItem(0,0)
+    for (i <- 0 to m) {
+      first = pq.dequeue()
+    }
+    pq.clear()
+    println(first.value)
+    solution(first.index) = true
     println(solution.mkString(","))
     for (i <- bestOption.indices) {
-      bestOption(i) = instance.serviceCost(i)(ind)
+      bestOption(i) = instance.serviceCost(i)(first.index)
     }
-    var seed = 0
     var goOn = true
-    var solValue = first
+    var solValue = first.value
     while (goOn){
-      var total = solValue
       var newFacVal = solValue
-      val potentialFac = ArrayBuffer[Double]()
       for (i <- solution.indices) {
         var objValue = solValue
         if (!solution(i)) {
-          objValue += instance.openCost(i)
           for (j <- bestOption.indices) {
+            objValue += instance.openCost(i)
             if (instance.serviceCost(j)(i) < bestOption(j))
               objValue -= (bestOption(j)-instance.serviceCost(j)(i))
           }
-          if (objValue < total)
-            potentialFac += objValue
-            else potentialFac += Double.MaxValue
+          if (objValue < solValue)
+            pq.enqueue(PQItem(i,objValue))
         }
       }
-      println(potentialFac.mkString(","))
-      val nSel = potentialFac.sorted.take(n)
-      println(nSel.mkString(","))
-      val rnd = Random(seed)
-      val chosen = rnd.uniform(n)
-      println(chosen)
-      newFacVal = nSel(chosen)
-      println(newFacVal)
-      val ind = potentialFac.indexOf(newFacVal)
-      if (newFacVal < total) {
-        total = newFacVal
+      val m = min(n, pq.size)
+      val chosen = rnd.uniform(m)
+      var pqSel = PQItem(0,0)
+      for (k <- 0 to chosen) {
+        pqSel = pq.dequeue()
+      }
+      newFacVal = pqSel.value
+      val ind = pqSel.index
+      pq.clear()
+      if (newFacVal < solValue) {
         solution(ind) = true
         for (i <- bestOption.indices) {
           if (instance.serviceCost(i)(ind) < bestOption(i))
@@ -64,33 +73,27 @@ class GRASP (val instance: Instance) {
         solValue = newFacVal
       }
       else goOn = false
-      seed += 1
       println(solution.mkString(","))
     }
     Solution.apply(solution)
   }
 
-
-
 }
 
 object GRASP {
 
-  def apply(instance: Instance): GRASP = {
-    new GRASP(instance)
+  def apply(instance: Instance, rnd: Random): GRASP = {
+    new GRASP(instance, rnd)
   }
 
 }
 
 object graspTest extends App {
   java.util.Locale.setDefault(java.util.Locale.ENGLISH)
-  //val inst = Instance.fromFile("instejemplo.txt")
-  //val instGrasp = GRASP(inst)
- // val sol = instGrasp.solve(2, 1)
-  //println(sol.eval(inst))
-  val inst2 = Instance.fromFileOrLib("cap102.txt")
-  val instGrasp2= GRASP(inst2)
+  val inst = Instance.fromFileOrLib("cap74.txt")
+  val rnd = new Random(0)
+  val instGrasp2= GRASP(inst, rnd)
   val sol2 = instGrasp2.solve(5)
-  println(sol2.eval(inst2))
+  println(sol2.eval(inst))
   println(sol2)
 }
