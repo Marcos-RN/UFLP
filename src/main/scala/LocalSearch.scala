eaa7ff07332cdfd1bf2c6138d3690542826f3628
Marcos-RN <marcosro1999@gmail.com>
2022-05-18 20:36:48 +0200

    Local Search Algorithm in progress

--- src/main/scala/LocalSearch.scala
+++ src/main/scala/LocalSearch.scala
@@ -1,6 +1,6 @@
 
-/*
-class LocalSearch(val instance: Instance, val solution: Solution) {
+
+/*class LocalSearch(val instance: Instance, val solution: Solution) {
 
   def neighbourSearch(bestOption : Array[Double]) : Solution = {
     var goOn = true
@@ -21,10 +21,13 @@ class LocalSearch(val instance: Instance, val solution: Solution) {
   def HillClimbing : Solution = {
     val bestOption = Array.ofDim[Double](instance.numCustomers)
     for (i <- bestOption.indices) {
-      bestOption(i) = instance.serviceCost(i)(0)
-      for (j <- 1 until instance.numLocations) {
-        if (instance.serviceCost(i)(j) < bestOption(i))
-          bestOption(i) = instance.serviceCost(i)(j)
+      bestOption(i) = Double.MaxValue
+      for (j <- 0 until instance.numLocations) {
+        if (solution.openFacilities(j)) {
+          if (instance.serviceCost(i)(j) < bestOption(i)) {
+            bestOption(i) = instance.serviceCost(i)(j)
+          }
+        }
       }
     }
     var objValue = solution.objectiveValue
@@ -34,11 +37,37 @@ class LocalSearch(val instance: Instance, val solution: Solution) {
 
       }
     }
-
-
-
-
     solution
   }
+}*/
+
+class LocalSearch(val instance: Instance, val solution: Solution) {
+
+  def changeLoc(i : Int) : Solution = {
+    val solnew = Solution.apply(solution.openFacilities, instance)
+    solnew.openFacilities(i) = !solution.openFacilities(i)
+    val objVal = Solution.eval(solnew.openFacilities, instance)
+    Solution.apply(solnew.openFacilities, objVal)
+  }
+
+
+  def HillClimbing : Solution = {
+    var objValue = solution.objectiveValue
+    var openFac = solution.openFacilities
+    var goOn = true
+    var c = 0
+    while (goOn && c < instance.numLocations) {
+      for (i <- solution.openFacilities.indices) {
+        if (changeLoc(i).objectiveValue < objValue) {
+          openFac = changeLoc(i).openFacilities
+          objValue = changeLoc(i).objectiveValue
+          goOn = false
+        }
+        c += 1
+      }
+    }
+    Solution.apply(openFac,objValue)
+  }
+
+
 }
-*/
