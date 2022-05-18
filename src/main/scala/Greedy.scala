567ac6a5a07dcc2ac9da9c3ac71592c85caf2248
Marcos-RN <marcosro1999@gmail.com>
2022-05-18 20:36:00 +0200

    Greedy OK. Comments added.

--- src/main/scala/Greedy.scala
+++ src/main/scala/Greedy.scala
@@ -1,11 +1,18 @@
+/* Implementation of Greedy Algorithm for solving ULFP.
+   instance is the problem to be solved.
+   */
 
 class Greedy (val instance: Instance) {
 
   def solve : Solution = {
+    // Current solution (all facilities closed initially)
     val solution = Array.ofDim[Boolean](instance.numLocations)
     val evalSol = Array.ofDim[Double](instance.numLocations)
+    // Array including the best options for each facility, it will be updated in every iteration
     val bestOption = Array.ofDim[Double](instance.numCustomers)
+    // Consider all singleton (with just one open facility) solutions
     for (i <- 0 until instance.numLocations) {
+      // Compute objective value of tentative solution
       var sum = 0.0
       for (j <- 0 until instance.numCustomers) {
         sum += instance.serviceCost(j)(i)
@@ -13,34 +20,41 @@ class Greedy (val instance: Instance) {
       sum += instance.openCost(i)
       evalSol(i) = sum
     }
+    // Chose the best singleton solution
     val first = evalSol.min
-    println(first)
     val ind = evalSol.indexOf(first)
     solution(ind) = true
-    println(solution.mkString(","))
+    // Update the bestOption array with the only open facility costs
     for (i <- bestOption.indices) {
       bestOption(i) = instance.serviceCost(i)(ind)
     }
+    // Define a sentinel value as a condition of termination (if false , then stop)
     var goOn = true
     var solValue = first
     while (goOn){
       var newFacVal = solValue
       var newFacInd = 0
+      // Try to open one of yet closed facilities
       for (i <- solution.indices) {
         if (!solution(i)) {
+          // Compute new objective value
           var objValue = solValue
           objValue += instance.openCost(i)
           for (j <- bestOption.indices) {
             if (instance.serviceCost(j)(i) < bestOption(j))
               objValue -= (bestOption(j)-instance.serviceCost(j)(i))
           }
+          // If it improves current solution, make it a tentative new solution
           if (objValue < newFacVal) {
             newFacVal = objValue
             newFacInd = i
           }
         }
       }
+      // If the objective value of the best solution computed above is better than the current one,
+      // open the facility that makes this value possible.
       if (newFacVal < solValue) {
+        // Update bestOptions and current solution for representing new current solution
         solution(newFacInd) = true
         for (i <- bestOption.indices) {
           if (instance.serviceCost(i)(newFacInd) < bestOption(i))
@@ -48,8 +62,8 @@ class Greedy (val instance: Instance) {
         }
         solValue = newFacVal
       }
+      // If no better value is possible using this procedure, while loop ends.
       else goOn = false
-      println(solution.mkString(","))
     }
     Solution.apply(solution, solValue)
   }
@@ -65,12 +79,13 @@ object Greedy {
 object greedyTest extends App {
   java.util.Locale.setDefault(java.util.Locale.ENGLISH)
   //val inst = Instance.fromFile("instejemplo2.txt")
-  //val instGreedy = Greedy(inst)
   //val sol = instGreedy.solve
   //println(sol.eval(inst))
-  // val inst2 = Instance.fromFileOrLib("cap104.txt")
+  val inst = Instance.fromFileOrLib("cap74.txt")
   val inst2 = Instance.random(0, 500, 150)
+  val instGreedy = Greedy(inst)
   val instGreedy2 = Greedy(inst2)
-  val sol2 = instGreedy2.solve
-  println(sol2)
+  val sol = instGreedy.solve
+  //val sol2 = instGreedy2.solve
+  println(sol)
 }
